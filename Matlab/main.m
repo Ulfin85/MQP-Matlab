33665ea8326a9e96535d3e143a10d73a1561f1d6
crxchow <cchow2@wpi.edu>
2023-12-12 20:34:25 -0500

    added joints 2-4

--- Matlab/main.m
+++ Matlab/main.m
@@ -1,11 +1,14 @@
 
 clear; close all;
 bot = robot;
-bot.get_Slist
-bot.get_M
+bot.get_Slist;
+bot.get_M;
 
 % define joints
-joint1 = 90;
+joint1 = 0;
+joint2 = 0;
+joint3 = 0;
+joint4 = 0;
 
 % load the bot data
 [smiData, Ts] = robot_data();
